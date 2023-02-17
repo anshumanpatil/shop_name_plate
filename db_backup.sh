fb6eb77f7265f949d3f3fba35f9ce47a4f7f31d0
APPLE <apple@APPLEs-MacBook-Pro.local>
2023-02-18 03:08:37 +0530

    added backups

--- db_backup.sh
+++ db_backup.sh
@@ -1,2 +1,4 @@
 export MARIADB_ROOT_PASSWORD=shop_name_plate
-docker-compose exec mariadb mysqldump -uroot -p${MARIADB_ROOT_PASSWORD} --all-databases > db_backup/mariadb-dump-$(date +%F_%H-%M-%S).sql
+bkp_name=$(uuidgen)
+docker-compose exec mariadb mysqldump -uroot -p${MARIADB_ROOT_PASSWORD} --all-databases > db_backup/${bkp_name}.sql
+echo $(date +%F_%H-%M-%S) ${bkp_name}.sql >> db_backup/backup_list.txt
